2317df4545bd63cc376c8c4f8ead12c6890e1161
Tammam Tautou <tautoutammam@gmail.com>
2023-06-08 14:31:23 -0700

    modify

--- server/index.js
+++ server/index.js
@@ -6,13 +6,19 @@ const path = require('path');
 const router = require('./routes');
 
 const app = express();
+const buildPath = path.join(__dirname, '../client/dist');
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
+app.use(express.static(buildPath));
+
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
 
 app.use('/', router);
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'));
+});
 
 const port = process.env.PORT || 3000;
 
