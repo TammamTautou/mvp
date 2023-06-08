685131d24e157b737195b5a5b0e3c6ed6585b1e5
Tammam Tautou <tautoutammam@gmail.com>
2023-06-08 14:39:19 -0700

    modify

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,9 +1,17 @@
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable import/no-named-as-default */
 /* eslint-disable import/extensions */
 import React from 'react';
 import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './components/App.jsx';
 
-render(
-  <App />,
-  document.getElementById('root'),
-);
+createRoot(document.getElementById('root'))
+  .render(
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<App />} />
+      </Routes>
+    </BrowserRouter>,
+  );
